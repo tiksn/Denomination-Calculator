94083bff70fed158a9f3b70ae1d22e64e4fe0766
Tigran TIKSN Torosyan <personal@tiksn.com>
2015-09-18 23:42:18 +0300

    initial version of parcer

---
+++ WhichWayToPay Parser/WhichWayToPay Parser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichWayToPay_Parser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.Clear();
+
+			Console.WriteLine("Done.");
+			Console.ReadLine();
+		}
+	}
+}
